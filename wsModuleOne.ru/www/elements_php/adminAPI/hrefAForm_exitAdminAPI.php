dad95a943d466ead6d82e63eb4e74c990953983e
Kaidor-s <ruslan.partizan.99@mail.ru>
2019-08-31 00:52:45 +0300

    Подключаемый элемент сайта для админа

---
+++ wsModuleOne.ru/www/elements_php/adminAPI/hrefAForm_exitAdminAPI.php
@@ -0,0 +1,5 @@
+<form action="../php/adminAPI/exitAdminAPI.php" method="post">
+	<input type="submit" class="authorization" value="Выйти&nbsp;с&nbsp;админки" name="done">
+</form>
+
+
---
+++ wsModuleOne.ru/www/elements_php/adminAPI/interfaceEddStory.php
@@ -0,0 +1,11 @@
+<?php
+	echo "<input type='submit' id='done_eDF_id' value='Изменить сюжет' name='done_eDF'/>";
+	//echo "<button id='done_eDF_id' name='done_eDF'>Изменить сюжет</button>";
+	echo "<form action='".$defaultAdress_EDFButt."' enctype='multipart/form-data' method='post' id='form_buttEDF'></form>";
+	//echo "<div id='form_buttEDF'></div>";
+	echo "<script src='".$defaultAdress_jsInterface."'></script>";
+?>
+
+
+
+
